eadfd3008776269abbf706c6a3dc0e84dea2de5d
cemCankaya <137628926+cemCankaya@users.noreply.github.com>
2024-06-15 20:12:01 +0300

    new commit

---
+++ week-2/1-conditionals.py
@@ -0,0 +1,26 @@
+# if statement
+
+firstNumber = 10
+
+if firstNumber > 5:
+    print(str(firstNumber) + " greater than 5")
+
+
+# if-else syntax
+secondNumber = 2
+
+if secondNumber > 5:
+    print(str(secondNumber) + " is greater than 5")
+else: 
+    print(str(secondNumber) + " is not greater than 5")
+
+# if-elif-else syntax
+
+thirdNumber = 6
+
+if thirdNumber > 10:
+    print(str(thirdNumber) + "is greater than 10")
+elif thirdNumber > 5:
+    print(str(thirdNumber) + " is greater than 5")
+else:
+    print(str(thirdNumber) + " is 5 or less")
