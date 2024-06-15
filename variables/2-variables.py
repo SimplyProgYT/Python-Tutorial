8f36314142119698b9cc2734a73afa1362254926
cemCankaya <137628926+cemCankaya@users.noreply.github.com>
2024-06-15 15:59:13 +0300

    new commit

---
+++ variables/2-variables.py
@@ -0,0 +1,10 @@
+# tpye() function
+
+firstName = "John"
+print(type(firstName)) # <class 'str'>
+
+age = 19
+print(type(age)) # <class 'int'>
+
+is_false = False
+print(type(is_false)) # <class 'bool'>
