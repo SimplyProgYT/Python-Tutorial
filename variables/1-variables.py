ddeac1921ed8f6c64aaec1edcf8eff3137951681
cemCankaya <137628926+cemCankaya@users.noreply.github.com>
2024-06-15 15:52:57 +0300

    new commit

---
+++ variables/1-variables.py
@@ -0,0 +1,13 @@
+# Declaring variables in python - Basic data types
+
+firstName = "John" # string type (str)
+print(firstName) # John
+
+age = 19 # integer type (int)
+print(age) # 19
+
+isTrue = True # boolean type (bool)
+print(isTrue) # True
+
+pi = 3.14 #float type
+print(pi) # 3.14
