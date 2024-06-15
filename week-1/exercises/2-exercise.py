6d5ed25a8fe9e4c533daa04ba8a148e9e04a38ca
cemCankaya <137628926+cemCankaya@users.noreply.github.com>
2024-06-15 17:24:17 +0300

    new commit

---
+++ week-1/exercises/2-exercise.py
@@ -0,0 +1,9 @@
+# Calculate the area of a rectangle
+
+length = input("Please enter the long side of the rectangle: ")
+width = input("Please enter the short side of the rectangle: ")
+
+
+area = int(length) * int(width)
+
+print("Area of the recatngle: " + str(area))
