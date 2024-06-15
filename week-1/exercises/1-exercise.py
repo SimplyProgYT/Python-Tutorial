e568dbd9067fd99a3dc6229f2959a1c643eb4253
cemCankaya <137628926+cemCankaya@users.noreply.github.com>
2024-06-15 17:19:32 +0300

    new commit

---
+++ week-1/exercises/1-exercise.py
@@ -0,0 +1,7 @@
+# Calculate the sum of two numbers
+
+firstNumber = input("Please enter an integer: ")
+secondNumber = input("Please enter an integer: ")
+
+sum = int(firstNumber) + int(secondNumber)
+print(sum) # 35
