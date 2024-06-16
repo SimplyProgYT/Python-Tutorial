cf011e798e4c3316fce60d1662bd212e3b5ab0d1
cemCankaya <137628926+cemCankaya@users.noreply.github.com>
2024-06-16 18:37:43 +0300

    new commit

---
+++ week-2/exercises/1-exercise.py
@@ -0,0 +1,13 @@
+# Addition of two numbers
+
+#! Take two numbers from the user
+
+num1 = int(input("Please enter the first integer :"))
+num2 = int(input("Please enter the second integer: "))
+
+#! Calculate the sum
+
+sum = num1 + num2
+
+#! Print the result
+print("The sum of", num1, "and", num2, "is", sum)
