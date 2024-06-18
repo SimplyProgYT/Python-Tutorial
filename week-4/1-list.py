35dbf478f1f85ff44b9bae8406f6f6f6b8208bc5
cemCankaya <137628926+cemCankaya@users.noreply.github.com>
2024-06-18 10:28:00 +0300

    new commit

---
+++ week-4/1-list.py
@@ -0,0 +1,14 @@
+# Declaring a list in python
+
+
+emptyList = []
+
+numbers = [1, 2, 3, 6, 21, 32]
+
+myList = [1, True, [1, 3, 5], "John"]
+
+print(myList) # [1, True, [1, 3, 5], 'John']
+print(numbers[3]) # 6
+numbers[3] = 123
+print(numbers[3]) # 123
+
