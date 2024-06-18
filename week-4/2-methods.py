b5e6f39c16b94c1736604a881817680d5eccefa3
cemCankaya <137628926+cemCankaya@users.noreply.github.com>
2024-06-18 10:36:36 +0300

    new commit

--- week-4/2-methods.py
+++ week-4/2-methods.py
@@ -6,14 +6,16 @@ fruits = ["apple", "banana"]
 #! append(), insert() and extend() methods
 
 
-#? append() method
+#? append() method -> Adding an item to the end of the list
+
 fruits.append("strawberry")
 print(fruits) # ['apple', 'banana', 'strawberry']
 
 
-#? insert() method  
+#? insert() method  -> Inserting an item at a specific position
 
 fruits.insert(1, "cherry")
 print(fruits) # ['apple', 'cherry', 'banana', 'strawberry']
 fruits.insert(3, "watermelon")
 print(fruits) # ['apple', 'cherry', 'banana', 'watermelon', 'strawberry']
+
