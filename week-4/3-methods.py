6b762e67f721d9dbeaa01b56ba562903ac9b25cc
cemCankaya <137628926+cemCankaya@users.noreply.github.com>
2024-06-18 10:42:14 +0300

    new commit

---
+++ week-4/3-methods.py
@@ -0,0 +1,11 @@
+# Removing items from a list
+
+# Example list
+fruits = ["apple", "banana", "strawberry"]
+
+#! remove(), pop() and clear() methods
+
+#? remove() method -> Removes a specific item
+
+fruits.remove("banana")
+print(fruits) # ['apple', 'strawberry']
