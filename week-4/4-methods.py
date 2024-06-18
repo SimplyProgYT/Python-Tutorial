4b06bd6f2115c839f779dcc0878929e338a136a3
cemCankaya <137628926+cemCankaya@users.noreply.github.com>
2024-06-18 10:46:06 +0300

    new commit

---
+++ week-4/4-methods.py
@@ -0,0 +1,14 @@
+# Example list
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Length of the list
+print(len(numbers))  # 7
+
+# Sorted version of the list
+print(sorted(numbers))  # [1, 1, 2, 3, 4, 5, 9]
+
+# Index of a specific item
+print(numbers.index(4))  # 2
+
+# Count of a specific item
+print(numbers.count(1))  # 2
