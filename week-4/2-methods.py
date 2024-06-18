3a8850913a6bc618a93a4c492228397c638052ce
cemCankaya <137628926+cemCankaya@users.noreply.github.com>
2024-06-18 10:32:08 +0300

    new commit

---
+++ week-4/2-methods.py
@@ -0,0 +1,11 @@
+# Adding items to a list
+
+# Example list
+fruits = ["apple", "banana"]
+
+#! append(), insert() and extend() methods
+
+
+#? append() method
+fruits.append("strawberry")
+print(fruits) # ['apple', 'banana', 'strawberry']
