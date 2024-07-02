db672978f73be3878191662fe0b4bfef212bec7b
SimplyProgYT <bcemcankaya@gmail.com>
2024-07-02 11:15:22 +0300

    new commit

--- week-1/1-variables.py
+++ week-1/1-variables.py
@@ -1,13 +1,13 @@
 # Declaring variables in python - Basic data types
 
-firstName = "John" # string type (str)
-print(firstName) # John
+first_name = "John" # string type (str)
+print(first_name) # John
 
 age = 19 # integer type (int)
 print(age) # 19
 
-isTrue = True # boolean type (bool)
-print(isTrue) # True
+is_true = True # boolean type (bool)
+print(is_true) # True
 
 pi = 3.14 #float type
 print(pi) # 3.14
